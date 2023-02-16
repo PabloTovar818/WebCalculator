d31cce7b175154569d769a8c7a38db06a121f06c
Pablo Tovar <109322891+PabloTovar818@users.noreply.github.com>
2023-02-16 14:56:45 -0500

    started js code: start function added, creating button vars
    deleted old html code from index file

--- WebCalculator/wwwroot/js/site.js
+++ WebCalculator/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function start() {
+
+    //create button variables
+    var num0 = document.getElementById("num0");
+    var num1 = document.getElementById("num1");
+    var num2 = document.getElementById("num2");
+    var num3 = document.getElementById("num3");
+    var num4 = document.getElementById("num4");
+    var num5 = document.getElementById("num5");
+    var num6 = document.getElementById("num6");
+    var num7 = document.getElementById("num7");
+    var num8 = document.getElementById("num8");
+    var num9 = document.getElementById("num9");
+    var clearKey = document.getElementById("clear-key");
+    var addKey = document.getElementById("add-key");
+    var subtractKey = document.getElementById("subtract-key");
+    var multiplyKey = document.getElementById("multiply-key");
+    var divisionKey = document.getElementById("division-key");
+
+    //listen for button presses
+    num0.addEventListener()
+}
+
+
+addEventListener("load", onload => {
+    start();
+}
